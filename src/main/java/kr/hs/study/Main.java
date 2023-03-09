4976426336b32b1722a3118d1aba2c794eca9f03
seoyeon-double-7 <s2103@e-mirim.hs.kr>
2023-03-09 15:10:26 +0900

    congif.xml 로딩 후 실행

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,7 +1,14 @@
 package kr.hs.study;
+import kr.hs.study.beans.TestBean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx =
+                new ClassPathXmlApplicationContext("config.xml");
+//  설정파일(config.xml)을 로딩하고 실행하는 것
+
+        ctx.close();
         System.out.println("Hello world!");
     }
 }
