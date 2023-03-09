a9f8b10232fdd48b41bab19e63f36d7bfee828b9
seoyeon-double-7 <s2103@e-mirim.hs.kr>
2023-03-09 15:46:33 +0900

    ⚡️: getbean()을 사용해서 객체 만들기

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,12 +1,27 @@
 package kr.hs.study;
 import kr.hs.study.beans.TestBean;
+import kr.hs.study.beans.TestBean2;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx =
                 new ClassPathXmlApplicationContext("config.xml");
+
+//  config.xml에서 객체 만들고 보관
 //  설정파일(config.xml)을 로딩하고 실행하는 것
+        TestBean obj1 = ctx.getBean("t1", TestBean.class);
+        System.out.println("obj1: " + obj1);
+
+        
+//        t2가지고 와서 obj2에 넣고 주소 출력
+
+        TestBean2 obj2 = ctx.getBean("t2", TestBean2.class);
+        System.out.println("obj2 : "+obj2);
+
+
+        TestBean obj3 = ctx.getBean("t3", TestBean.class);
+        System.out.println("obj3 : "+obj3);
 
         ctx.close();
         System.out.println("Hello world!");
